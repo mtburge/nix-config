51b0c8bd782f87e0e8beb562227c74fb3e6db4cc
Matt Burgess <hey@mtburge.com>
2024-03-18 22:46:25 +0000

    lockscreen ui

--- user/desktop-environment/hyprland/hyprlock.nix
+++ user/desktop-environment/hyprland/hyprlock.nix
@@ -6,52 +6,82 @@
   ];
   
   home.file.".config/hypr/hyprlock.conf".text = ''
+    general {
+      grace = 0
+      disable_loading_bar = true
+    }
+
     background {
       monitor =
       path = ~/.dotfiles/wallpapers/skipping-stones.png
-      blur_size = 7
+      blur_size = 14
       blur_passes = 1
       noise = 0.0117
     }
 
-    input-field {
+    label {
       monitor =
-      size = 300, 50
-      outline_thickness = 3
-      dots_size = 0.20 # Scale of input-field height, 0.2 - 0.8
-      dots_spacing = 0.15 # Scale of dots' absolute size, 0.0 - 1.0
-      dots_center = true
-      dots_rounding = -1 # -1 default circle, -2 follow input-field rounding
-      outer_color = rgb(30, 30, 46)
-      inner_color = rgb(88, 91, 112)
-      font_color = rgb(200, 200, 200)
-      fade_on_empty = false
-      fade_timeout = 1000 # Milliseconds before fade_on_empty is triggered.
-      placeholder_text =
-      hide_input = false
-      rounding = -1 # -1 means complete rounding (circle/oval)
-      check_color = rgb(204, 136, 34)
-      fail_color = rgb(204, 34, 34) # if authentication failed, changes outer_color and fail message color
-      fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i> # can be set to empty
-      fail_transition = 300 # transition time in ms between normal outer_color and fail_color
-      capslock_color = -1
-      numlock_color = -1
-      bothlock_color = -1 # when both locks are active. -1 means don't change outer color (same for above)
-      invert_numlock = false # change color if numlock is off
-
-      position = 0, 10
+      text = cmd[update:1000] echo "<span letter_spacing='1' weight='bold'>$TIME</span>"
+      color = rgba(200, 200, 200, 1)
+      font_size = 96
+      font_family = JetBrains Mono
+      font_weight = bold
+      letter_spacing = 0.5
+      position = 0, -220
       halign = center
-      valign = center
+      valign = top
+    }
+    
+    label {
+      monitor =
+      text = cmd[update:1000] echo "$(date +"%a %d %B")"
+      color = rgba(200, 200, 200, .7)
+      font_size = 24
+      font_family = JetBrains Mono
+      position = 0, -180
+      halign = center
+      valign = top
     }
 
     label {
       monitor =
       text = $USER
-      color = rgba(200, 200, 200, 1.0)
-      font_size = 25
-      font_family = Noto Sans
+      color = rgba(200, 200, 200, .7)
+      font_size = 18
+      font_family = JetBrains Mono
 
-      position = 0, 80
+      position = 0, 55
+      halign = center
+      valign = center
+    }
+    
+    input-field {
+      monitor =
+      size = 250, 50
+      outline_thickness = 0
+      dots_size = 0.2 # Scale of input-field height, 0.2 - 0.8
+      dots_spacing = 0.2 # Scale of dots' absolute size, 0.0 - 1.0
+      dots_center = true
+      dots_rounding = -1 # -1 default circle, -2 follow input-field rounding
+      outer_color = rgb(30, 30, 46)
+      inner_color = rgb(69, 71, 90)
+      font_color = rgb(200, 200, 200)
+      fade_on_empty = false
+      font_family = JetBrains Mono
+      fade_timeout = 1000 # Milliseconds before fade_on_empty is triggered.
+      placeholder_text = <span foreground="##9399b2">Password</span>
+      hide_input = false
+      rounding = 0.25 # -1 means complete rounding (circle/oval)
+      check_color = rgb(204, 136, 34)
+      fail_color = rgb(243, 139, 168) # if authentication failed, changes outer_color and fail message color
+      fail_text = <i>$FAIL <b>($ATTEMPTS)</b></i> # can be set to empty
+      fail_transition = 120
+      capslock_color = -1
+      numlock_color = -1
+      bothlock_color = -1 # when both locks are active. -1 means don't change outer color (same for above)
+      invert_numlock = false # change color if numlock is off
+
+      position = 0, 0
       halign = center
       valign = center
     }
