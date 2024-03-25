91c8bdf5a01adda7cd8d77736cfc09c7266fc1b8
Matt Burgess <hey@mtburge.com>
2024-03-25 20:36:57 +0000

    firefox prefs

--- user/firefox.nix
+++ user/firefox.nix
@@ -11,6 +11,13 @@
         "browser.urlbar.suggest.quicksuggest.sponsored" = false; # don't suggest sponsored links
         #"signon.autofillForms" = false; # disable password manager
         "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org"; # dark theme
+        "browser.newtabpage.activity-stream.default.sites" = ""; # only show sites i've visited
+        "browser.newtabpage.activity-stream.discoverystream.enabled" = ""; # clean newtab page
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = false; # clean newtab page
+        "browser.newtabpage.activity-stream.topSitesRows" = "2"; # two rows of sites
+        "browser.newtabpage.activity-stream.section.highlights.includePocket" = false; # remove pocket
+        "extensions.pocket.enabled" = false; # remove pocket
+        "dom.event.contextmenu.enabled" = true; # i'll right click whereever i like, thank you
       };
       
       extensions = with config.nur.repos.rycee.firefox-addons; [
