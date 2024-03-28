e8c4c3653d8603bcfc5dfe215885f49c28898cb8
Matt Burgess <hey@mtb.dev>
2024-03-28 11:18:41 +0000

    switch power profile based on battery state

--- system/udev.nix
+++ system/udev.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   services.udev.extraRules = ''
+    # allow keyboard to wake from suspend
     ACTION=="add", SUBSYSTEM=="usb", DRIVER=="usb", ATTR{power/wakeup}="enabled"
+  
+    # switch to balanced performance when plugged in
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="1", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set balanced"
+    
+    # switch to power-saver whilst on battery
+    SUBSYSTEM=="power_supply", ENV{POWER_SUPPLY_ONLINE}=="0", RUN+="${pkgs.power-profiles-daemon}/bin/powerprofilesctl set power-saver"
   '';
 }
