f5fbd47212fc7e94f8c946aa6185c2a53b71cd16
Matt Burgess <hey@mtb.dev>
2024-04-03 23:08:10 +0100

    add kitty config

--- user/apps/kitty.nix
+++ user/apps/kitty.nix
@@ -4,5 +4,15 @@
   programs.kitty = {
     enable = true;
     shellIntegration.enableFishIntegration = true;
+    theme = "Catppuccin-Mocha";
+    settings = {
+      scrollback_lines = 10000;
+      font_family = "JetBrains Mono";
+      font_size = 12;
+      modify_font = "cell_height 100%";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      background = "#181825";
+    };
   };
 }
