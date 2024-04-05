c685d2ce2ec69ef37a5e6a586a41011769bc29c9
Matt Burgess <hey@mtb.dev>
2024-04-05 14:57:25 +0100

    migrated git config

--- user/shell/git.nix
+++ user/shell/git.nix
@@ -9,6 +9,22 @@
     userEmail = user.email;
     extraConfig = {
       init.defaultBranch = "main";
+      core.excludesfile= "~/.gitignore_global";
+
+      alias = {
+        # quick status
+        "s" = "status -s";
+
+        # pretty log for recent commits
+        "l" = "log --pretty=oneline -n 20 --graph --abbrev-commit";
+
+        # contributors
+        "contrib" = "shortlog --summary --numbered";
+      };
     };
   };
+
+  home.file.".gitignore_global".text = ''
+    *.swp
+  '';
 }
