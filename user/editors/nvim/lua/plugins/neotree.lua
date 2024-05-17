132b8a820f624c1835fa9c291734116a938a876a
Matt Burgess <hey@mtb.dev>
2024-05-17 22:09:03 +0100

    add neotree

--- user/editors/nvim/lua/plugins/neotree.lua
+++ user/editors/nvim/lua/plugins/neotree.lua
@@ -1,9 +1,12 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	}
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    vim.keymap.set('n', '<leader>fe', '<Cmd>Neotree toggle<CR>', { desc = '[f]ile [e]xplorer' })
+  end
 }
